69985b53349aafc18f3d91e430c4be4003193262
Shine Bobby <upshinebobby@gmail.com>
2016-10-09 21:41:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,11 @@ element.innerHTML = "New Value";
 // Move Image
 
 var img = document.getElementById("madi");
-
+var marginLeft = 0;
+function moveRight(){
+  marginLeft = marginLeft + 1;
+  img.style.marginLeft = marginLeft + 'px';
+}
 img.onClick = function(){
-    img.style.marginLeft = '200px';
+    var interval = setInterval(moveRight, 50);
 };
