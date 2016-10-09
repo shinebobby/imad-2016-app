622acdcef429d86c6837dd127c1c4fefe97e0106
Shine Bobby <upshinebobby@gmail.com>
2016-10-09 21:55:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,6 @@ function moveRight(){
   marginLeft = marginLeft + 1;
   img.style.marginLeft = marginLeft + 'px';
 }
-img.onClick = function(){
+img.onclick = function(){
    var interval = setInterval(moveRight, 50);
 };
