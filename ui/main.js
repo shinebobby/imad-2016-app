2c6e9d394eab8a8e626cca7374e502ec5c614799
Shine Bobby <upshinebobby@gmail.com>
2016-10-09 20:22:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,10 @@
 console.log('Loaded!');
 
-// 
+// Change the Main Text Div
 var element = document.getElementById("main-text");
 
 element.innerHTML = "New Value";
+
+// Move Image
+
+var img = document.getElementById("img");
