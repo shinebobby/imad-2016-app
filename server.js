774f607edea5dc758d3353dacca7ce88b30a1b04
Shine Bobby <upshinebobby@gmail.com>
2016-10-11 10:20:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,6 +95,14 @@ app.get('/article-two', function (req, res) {
 app.get('/article-three', function (req, res) {
    res.send(createTemplate(articleThree));
 });
+
+counter = 0;
+app.get('/counter', function(req, res){
+   counter=counter +1;
+   res.send(counter.toString());
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
